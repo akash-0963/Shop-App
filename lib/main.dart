60da1a548debf2cca53d531fc407575600c97081
akash-0963 <akashjare09@gmail.com>
2023-07-01 10:43:47 +0530

    Registration and Login

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:shop_app/firebase_options.dart';
+import 'package:shop_app/pages/home_page.dart';
+import 'package:shop_app/pages/login_page.dart';
+import 'package:shop_app/pages/signup_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/':(context) => HomePage(),
+        '/login':(context) => LoginPage(),
+        '/signup':(context) => SignupPage(),
+      },
+    );
+  }
+}
